157b6ba0e822893ca583e4c05146c7f419a7f38f
Glickman <glickman1@mail.tau.ac.il>
2021-03-11 23:47:13 +0200

    models

---
+++ models/model_loading.py
@@ -0,0 +1,13 @@
+from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
+
+
+def get_bart_model_and_tokenizer():
+    # model = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
+    model = BartForConditionalGeneration.from_pretrained('sshleifer/distilbart-cnn-12-6')
+    # tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
+    tokenizer = BartTokenizer.from_pretrained('sshleifer/distilbart-cnn-12-6')
+    return model, tokenizer
+
+
+model, tokenizer = get_bart_model_and_tokenizer()
+print(model)

cc60941791ef17a5fde83e4f271a16bc90e94b46
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 17:47:17 +0200

    fast tokenizer

--- models/model_loading.py
+++ models/model_loading.py
@@ -6,8 +6,8 @@ def get_bart_model_and_tokenizer():
     model = BartForConditionalGeneration.from_pretrained('sshleifer/distilbart-cnn-12-6')
     # tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
     tokenizer = BartTokenizer.from_pretrained('sshleifer/distilbart-cnn-12-6',
-                                              force_bos_token_to_be_generated=True
-                                              # use_fast=True,
+                                              force_bos_token_to_be_generated=True,
+                                              use_fast=True,
                                               # return_token_type_ids=True
                                               )
 

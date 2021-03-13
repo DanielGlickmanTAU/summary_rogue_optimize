dff58dcb7fa1da8c7ea67ff4ee9df95175324f03
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 22:05:17 +0200

    shorter

--- models/tokenize.py
+++ models/tokenize.py
@@ -1,10 +1,10 @@
 import torch
 
 
-def tokenize(tokenizer, texts, max_length=1024):
+def tokenize(tokenizer, texts, max_length=512):
     tokenized = tokenizer(texts,
                           max_length=max_length,
-                          return_tensors='pt', padding=True, truncation=True)
+                          return_tensors='pt', padding="max_length", truncation=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     return tokenized.to(device)

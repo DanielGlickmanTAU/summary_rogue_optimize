faa9709f81624c5765c4698d143e6bbb877d1cb2
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 18:45:36 +0200

    move tensors to gpu

--- models/tokenize.py
+++ models/tokenize.py
@@ -1,6 +1,8 @@
+import torch
+
+
 def tokenize(tokenizer, texts, max_length=1024):
-    return tokenizer(texts,
-                     max_length=max_length,
-                     return_tensors='pt',
-                     padding=True,
-                     truncation=True)
+    tokenized = tokenizer(texts, max_length=max_length, return_tensors='pt', padding=True, truncation=True)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    return {'input_ids': tokenized['input_ids'].to(device), 'attention_mask': tokenized['attention_mask'].to(device)}

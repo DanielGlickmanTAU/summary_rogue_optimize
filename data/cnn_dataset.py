2740675ba7057a0585208452d0afa69a5e3c9347
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 16:00:24 +0200

    work with part of the dataset right now

--- data/cnn_dataset.py
+++ data/cnn_dataset.py
@@ -1,5 +1,7 @@
 import datasets
 
 
-def get_cnn_dataset():
+def get_cnn_dataset(subset: int = None):
+    if subset:
+        return datasets.load_dataset('cnn_dailymail', '3.0.0').select(range(subset))
     return datasets.load_dataset('cnn_dailymail', '3.0.0')

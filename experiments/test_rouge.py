c267a4132be50512517db1221720c3c3d1c7af19
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 19:54:37 +0200

    sanity check

--- experiments/test_rouge.py
+++ experiments/test_rouge.py
@@ -2,10 +2,11 @@ from models import model_loading, tokenize, generate
 from data import cnn_dataset, metrics
 
 batch_size = 16
-train_examples = 32
+train_examples = 16 * 2
+validation_examples = 16 * 10
 
 model, tokenizer = model_loading.get_bart_model_and_tokenizer()
-cnn = cnn_dataset.get_cnn_dataset(subset=train_examples)
+cnn = cnn_dataset.get_cnn_dataset(train_subset=train_examples, valid_subset=validation_examples)
 rouge = metrics.get_rouge()
 
 
@@ -22,7 +23,13 @@ def add_summary_and_rouge(examples):
     return {'generated_summaries': generated_summaries, 'rouge2': rouge2, 'rouge1': rouge1}
 
 
-dataset = cnn['train'].map(add_summary_and_rouge, batched=True, batch_size=batch_size)
+dataset = cnn['validation'].map(add_summary_and_rouge, batched=True, batch_size=batch_size)
+
+print('rouge1 at first 5', sum(dataset['rouge2'][:5]) / 5)
+print('rouge2 at first 5', sum(dataset['rouge1'][:5]) / 5)
+
+print('rouge1 at first 16', sum(dataset['rouge2'][:16]) / 16)
+print('rouge2 at first 16', sum(dataset['rouge1'][:16]) / 16)
 
 print('rouge1', sum(dataset['rouge2']) / len(dataset['rouge2']))
 print('rouge2', sum(dataset['rouge1']) / len(dataset['rouge1']))

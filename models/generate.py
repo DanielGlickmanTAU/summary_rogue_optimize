90ebed138f1fb8a029d8fcdf1547da0d61db5b10
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 18:31:15 +0200

    minor

--- models/generate.py
+++ models/generate.py
@@ -5,7 +5,7 @@ def summarize(model, tokenizer, texts):
     """input is list of strings batch
         output is list of strings"""
     inputs = tokenize.tokenize(tokenizer, texts)
-    print('generating ', len(inputs['input_ids']), ' summaries')
+    print('generating', len(inputs['input_ids']), 'summaries')
     summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=1024)
     return [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False)
             for g in summary_ids]

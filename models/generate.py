765a9f2656123263c9c07b5f4b941e3e4aac4203
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 19:12:03 +0200

    pass input properly

--- models/generate.py
+++ models/generate.py
@@ -6,6 +6,6 @@ def summarize(model, tokenizer, texts):
         output is list of strings"""
     inputs = tokenize.tokenize(tokenizer, texts)
     print('generating', len(inputs['input_ids']), 'summaries')
-    summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=1024)
+    summary_ids = model.generate(**inputs, num_beams=4, max_length=1024)
     return [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False)
             for g in summary_ids]

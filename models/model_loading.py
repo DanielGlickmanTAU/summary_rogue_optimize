540f36f665868557de87cf5e2f1af1894803d62a
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 16:00:31 +0200

    fix model config

--- models/model_loading.py
+++ models/model_loading.py
@@ -6,4 +6,6 @@ def get_bart_model_and_tokenizer():
     model = BartForConditionalGeneration.from_pretrained('sshleifer/distilbart-cnn-12-6')
     # tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
     tokenizer = BartTokenizer.from_pretrained('sshleifer/distilbart-cnn-12-6')
+
+    model.resize_token_embeddings(len(tokenizer))
     return model, tokenizer

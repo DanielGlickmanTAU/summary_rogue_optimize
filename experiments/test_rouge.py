68621a78d95f285d41cbf2384bd85366011d4457
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 19:32:25 +0200

    use both rouge-1 and 2

--- experiments/test_rouge.py
+++ experiments/test_rouge.py
@@ -1,9 +1,8 @@
 from models import model_loading, tokenize, generate
 from data import cnn_dataset, metrics
-import torch
 
 model, tokenizer = model_loading.get_bart_model_and_tokenizer()
-cnn = cnn_dataset.get_cnn_dataset(subset=2)
+cnn = cnn_dataset.get_cnn_dataset(subset=20)
 rouge = metrics.get_rouge()
 
 
@@ -19,13 +18,15 @@ def add_summary_and_rouge(examples):
     assert len(gold) == len(generated_summaries)
     scores = [metrics.calc_score(pred, ref) for pred, ref in zip(generated_summaries, gold)]
     rouge2 = [x['rouge-2'] for x in scores]
+    rouge1 = [x['rouge-1'] for x in scores]
 
-    return {'generated_summaries': generated_summaries, 'rouge2': rouge2}
+    return {'generated_summaries': generated_summaries, 'rouge2': rouge2, 'rouge1': rouge1}
 
 
-dataset = cnn['train'].map(add_summary_and_rouge)
+dataset = cnn['train'].map(add_summary_and_rouge, batched=True)
 
-print(dataset[0])
+print('rouge1', sum(dataset['rouge2']) / len(dataset['rouge2']))
+print('rouge2', sum(dataset['rouge1']) / len(dataset['rouge1']))
 
 # print(decoded_summary_generated)
 #

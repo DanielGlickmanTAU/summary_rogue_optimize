a9c7d0d450102f6fa0b2d2ad0f0c557a96fa83e0
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 19:12:15 +0200

    move to gpu

--- models/tokenize.py
+++ models/tokenize.py
@@ -2,7 +2,9 @@ import torch
 
 
 def tokenize(tokenizer, texts, max_length=1024):
-    tokenized = tokenizer(texts, max_length=max_length, return_tensors='pt', padding=True, truncation=True)
+    tokenized = tokenizer(texts,
+                          max_length=max_length,
+                          return_tensors='pt', padding=True, truncation=True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    return {'input_ids': tokenized['input_ids'].to(device), 'attention_mask': tokenized['attention_mask'].to(device)}
+    return tokenized.to(device)

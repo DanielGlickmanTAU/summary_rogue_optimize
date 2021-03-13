6d62ddfa736c490b1cbaa05a5fda93fa93e883b8
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 18:07:23 +0200

    generation

--- data/metrics.py
+++ data/metrics.py
@@ -11,3 +11,16 @@ def rouge_aggregate_score_to_rouge1_mid(aggregate_score):
 
 def rouge_aggregate_score_to_rouge2_mid(aggregate_score):
     return aggregate_score['rouge2'].mid.fmeasure
+
+
+def rouge_aggregate_score_to_rougel_mid(aggregate_score):
+    return aggregate_score['rougel'].mid.fmeasure
+
+
+rouge = get_rouge()
+
+
+def calc_score(prediction, gold):
+    score = rouge.compute(predictions=prediction, references=gold)
+    return {'rouge-1': rouge_aggregate_score_to_rouge1_mid(score),
+            'rouge-2': rouge_aggregate_score_to_rouge2_mid(score)}

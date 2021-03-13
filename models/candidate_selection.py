9b5e75d824dfce8bd06281f957aae8ca53ffec68
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 20:42:34 +0200

    fixes

--- models/candidate_selection.py
+++ models/candidate_selection.py
@@ -14,3 +14,21 @@ def select_best(dataset, k=0.15, scale_exponent=1., metric='rouge2', force_stop_
                                   replace=False)  # cant replace because using datasets.select which may use unique indexs
 
     return dataset.select(indexes)
+
+
+def select_best(dataset, k=0.15, scale_exponent=1., metric='rouge2', ignore_assertions=False):
+    if not ignore_assertions:
+        assert scale_exponent >= 1.  # we might want stronger ones to show more, but not the other way around
+        assert k <= 0.9 or k > 1  # giving a precentile too large makes no sense
+    if k < 1:
+        k = int(len(dataset) * k)
+    print('taking top ', k)
+    weights = dataset[metric]
+    indexes = numpy.random.choice(
+        list(range(0, len(dataset))),
+        k,
+        p=weights,
+        replace=False  # cant replace because using datasets.select which may use unique indexs
+    )
+
+    return dataset.select(indexes)

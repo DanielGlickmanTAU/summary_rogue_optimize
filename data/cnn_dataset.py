ce525f57364087ad5868c8da8e507ee84580a4b2
Glickman <glickman1@mail.tau.ac.il>
2021-03-13 19:48:50 +0200

    allow controlling the size of validation and test sets..

--- data/cnn_dataset.py
+++ data/cnn_dataset.py
@@ -1,9 +1,12 @@
 import datasets
 
 
-def get_cnn_dataset(subset: int = None):
+def get_cnn_dataset(train_subset: int = None, valid_subset: int = None, test_subset: int = None):
     dataset = datasets.load_dataset('cnn_dailymail', '3.0.0')
-    if subset:
-        dataset['train'] = dataset['train'].select(range(subset))
-        return dataset
+    if train_subset:
+        dataset['train'] = dataset['train'].select(range(train_subset))
+    if valid_subset:
+        dataset['validation'] = dataset['validation'].select(range(valid_subset))
+    if train_subset:
+        dataset['test'] = dataset['test'].select(range(test_subset))
     return dataset

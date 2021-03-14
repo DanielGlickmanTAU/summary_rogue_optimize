c4632d8b7dcf83ae3443c5120ba7895ccbd1cadf
Glickman <glickman1@mail.tau.ac.il>
2021-03-14 14:55:18 +0200

    log num return sequences

--- models/generate.py
+++ models/generate.py
@@ -4,6 +4,7 @@ top_p = 0.92
 top_k = 200
 num_beams = 4
 do_sample = True
+num_return_sequences = 1
 
 
 def summarize(model, tokenizer, texts):
@@ -18,7 +19,7 @@ def summarize(model, tokenizer, texts):
                                  top_p=top_p,
                                  top_k=top_k,
                                  max_length=200,
-                                 num_return_sequences=1
+                                 num_return_sequences=num_return_sequences
 
                                  )
     return [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False)

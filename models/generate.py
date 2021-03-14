a37d9df28c845d75537d3210cdc1ba9e41a2a928
Glickman <glickman1@mail.tau.ac.il>
2021-03-14 12:18:40 +0200

    tidy up

--- models/generate.py
+++ models/generate.py
@@ -6,6 +6,15 @@ def summarize(model, tokenizer, texts):
         output is list of strings"""
     inputs = tokenize.tokenize(tokenizer, texts)
     print('generating', len(inputs['input_ids']), 'summaries')
-    summary_ids = model.generate(**inputs, num_beams=4, max_length=1024)
+    summary_ids = model.generate(**inputs,
+                                 # num_beams=4,
+                                 do_sample=True,
+                                 # max_length=50,
+                                 top_p=0.92,
+                                 top_k=200,
+                                 max_length=200,
+                                 num_return_sequences=1
+
+                                 )
     return [tokenizer.decode(g, skip_special_tokens=True, clean_up_tokenization_spaces=False)
             for g in summary_ids]

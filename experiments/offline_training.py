4fb9a3518115e4febc3418445cf549dbf11a6ef8
Glickman <glickman1@mail.tau.ac.il>
2021-03-14 12:49:43 +0200

    fix file name

--- experiments/offline_training.py
+++ experiments/offline_training.py
@@ -2,5 +2,5 @@ import os
 import sys
 
 os.system("nohup sh -c '" +
-          sys.executable + " test_rouge.py.py >res1.txt " +
+          sys.executable + " test_rouge.py > res1.txt " +
           "' &")

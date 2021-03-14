5e8e549087fc9fe7a4ca1824f4e7fdd691e7b5c5
Glickman <glickman1@mail.tau.ac.il>
2021-03-14 12:18:03 +0200

    experiment.py

---
+++ experiments/experiment.py
@@ -0,0 +1,30 @@
+import collections
+from comet_ml import Experiment
+
+
+def start_experiment(tags=None, hyperparams=None):
+    def flatten(d):
+        items = []
+        for k, v in d.items():
+            if isinstance(v, collections.MutableMapping):
+                if hasattr(v, '__dict__'):
+                    items.extend(flatten(v.__dict__).items())
+                else:
+                    items.extend(flatten(v).items())
+            else:
+                items.append((k, v))
+        return dict(items)
+
+    if hyperparams is None:
+        hyperparams = {}
+
+    if tags is None:
+        tags = []
+
+    experiment = Experiment('FvAd5fm5rJLIj6TtmfGHUJm4b', project_name='summary-sampling', workspace="danielglickmantau")
+    if len(tags):
+        experiment.add_tags(tags)
+    if len(hyperparams):
+        experiment.log_parameters(flatten(hyperparams))
+
+    return experiment

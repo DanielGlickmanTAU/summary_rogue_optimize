b2952530052e6c6d605584af09bf02307b415849
Glickman <glickman1@mail.tau.ac.il>
2021-03-15 14:20:26 +0200

    extract model name variable

--- models/model_loading.py
+++ models/model_loading.py
@@ -1,12 +1,16 @@
 import torch
 from transformers import BartTokenizer, BartForConditionalGeneration, BartConfig
 
+#model_name = 'facebook/bart-large'
+# model_name = 'sshleifer/distilbart-6-6-cnn'
+model_name = 'sshleifer/distilbart-cnn-12-6'
+
 
 def get_bart_model_and_tokenizer():
-    # model = BartForConditionalGeneration.from_pretrained('facebook/bart-large')
-    model = BartForConditionalGeneration.from_pretrained('sshleifer/distilbart-cnn-12-6')
+    # model = BartForConditionalGeneration.from_pretrained(model_name)
+    model = BartForConditionalGeneration.from_pretrained(model_name)
     # tokenizer = BartTokenizer.from_pretrained('facebook/bart-large')
-    tokenizer = BartTokenizer.from_pretrained('sshleifer/distilbart-cnn-12-6',
+    tokenizer = BartTokenizer.from_pretrained(model_name,
                                               # force_bos_token_to_be_generated=True,
                                               use_fast=True,
                                               )

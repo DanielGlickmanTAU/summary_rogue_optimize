bd173715666b5e0d56b372fe4bf59d0cbe464ab7
Glickman <glickman1@mail.tau.ac.il>
2021-04-19 10:08:41 +0300

    clean some stuff

--- experiments/test_rouge.py
+++ experiments/test_rouge.py
@@ -129,12 +129,6 @@ precentile = 0.06
 model, tokenizer = model_loading.get_bart_model_and_tokenizer()
 cnn = data_loading.get_xsum_dataset(train_subset=train_examples, valid_subset=validation_examples)
 
-# search_validation_loss(cnn[validation_split],
-#                        do_sample=True, top_p=0.9, top_k=100, num_beams=5, num_return_sequences=10)
-# search_validation_loss(cnn[validation_split],
-
-#                        do_sample=True, top_p=0.9, top_k=100, num_beams=10, num_return_sequences=10, batch_size=8)
-
 search_validation_loss(cnn[validation_split],
                        do_sample=True, top_p=0.9, top_k=100, num_beams=3, num_return_sequences=3, batch_size=16)
 search_validation_loss(cnn[validation_split],

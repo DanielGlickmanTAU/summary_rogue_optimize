64baa242b251a66863372e4ff15575aa727b8275
Glickman <glickman1@mail.tau.ac.il>
2021-04-23 23:01:07 +0300

    calculate repeat

--- experiments/flows.py
+++ experiments/flows.py
@@ -7,6 +7,7 @@ from datasets import load_from_disk
 from data.metrics import calc_score_avg_best_first_for_list_of_summaries
 from models import generate
 from models.generate import SearchParams
+import math
 
 
 def get_by_key(list_of_dicts, key):
@@ -17,7 +18,9 @@ def add_summary_and_rouge(model, tokenizer, examples, search_params: SearchParam
     articles = examples['article']
     gold = examples['highlights']
     if search_params.do_sample:
-        generated_summaries = repeat_p_search(articles, model, search_params, tokenizer)
+        # can fit like 8 beams in a time
+        repeat = math.ceil(search_params.num_beams / 8)
+        generated_summaries = repeat_p_search(articles, model, search_params, tokenizer, repeat=repeat)
     else:
         generated_summaries = generate.summarize(model, tokenizer, articles, search_params)
 

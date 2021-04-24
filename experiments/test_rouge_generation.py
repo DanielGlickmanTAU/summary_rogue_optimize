074b7121c59a5ef844aab81e5d50148ce4f917a9
Glickman <glickman1@mail.tau.ac.il>
2021-04-24 09:03:19 +0300

    refactor

--- experiments/test_rouge_generation.py
+++ experiments/test_rouge_generation.py
@@ -1,24 +1,22 @@
 from experiments.flows import search_validation_loss
 from data import data_loading
+from flows import loading
 from models import model_loading
 from models.generate import PSearchParams, BeamSearchParams
 
 batch_size = 2
-train_examples = 1
-validation_examples = 1200
+train_examples = 50_000
+validation_examples = 1
 dataset = 'xsum'
-search_params = BeamSearchParams(num_beams=32, num_return_sequences=32)
-search_params = PSearchParams(num_beams=64, num_return_sequences=64, top_p=0.9)
+# search_params = BeamSearchParams(num_beams=32, num_return_sequences=32)
+
+
+search_params = PSearchParams(num_beams=8, num_return_sequences=8, top_p=0.9)
 
 
 def _generate():
-    if dataset == 'xsum':
-        model, tokenizer = model_loading.get_bart_model_and_tokenizer_xsum()
-        cnn = data_loading.get_xsum_dataset(train_subset=train_examples, valid_subset=validation_examples)
-    else:
-        model, tokenizer = model_loading.get_bart_model_and_tokenizer_cnn()
-        cnn = data_loading.get_cnn_dataset(train_subset=train_examples, valid_subset=validation_examples)
-    search_validation_loss(cnn['validation'], model, tokenizer, search_params, batch_size)
+    cnn, model, tokenizer = loading.load_dataset_model_tokenizer(dataset, train_examples, validation_examples)
+    search_validation_loss(cnn['train'], model, tokenizer, search_params, batch_size)
 
 
 _generate()

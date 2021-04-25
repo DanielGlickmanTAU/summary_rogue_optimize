0416911c94ce289f3171d4e5d4f85f249bad98b7
Glickman <glickman1@mail.tau.ac.il>
2021-04-25 17:33:36 +0300

    xsum setup

--- experiments/search_param_setups.py
+++ experiments/search_param_setups.py
@@ -11,6 +11,16 @@ def get_xsum_beam_train_setup():
     return dataset_name, split, train_examples, validation_examples, search_params, batch_size
 
 
+def get_xsum_beam_validation_setup():
+    dataset_name = 'xsum'
+    split = 'validation'
+    train_examples = 1
+    validation_examples = 10_000
+    search_params = BeamSearchParams(num_beams=8, num_return_sequences=8)
+    batch_size = 2
+    return dataset_name, split, train_examples, validation_examples, search_params, batch_size
+
+
 def get_xsum_psearch_train_setup():
     dataset_name = 'xsum'
     split = 'train'

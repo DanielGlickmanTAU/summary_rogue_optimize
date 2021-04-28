f4881bf415e27cbd03fde43757fb0c3920644440
Glickman <glickman1@mail.tau.ac.il>
2021-04-28 13:17:53 +0300

    normalize labels

--- models/RankerModel.py
+++ models/RankerModel.py
@@ -5,40 +5,25 @@ import torch
 from torch.nn import CrossEntropyLoss, MSELoss
 
 
-@dataclass
-class Holder:
-    data: object
-
-
 class RankerModel(nn.Module):
     def __init__(self, roberta):
         super(RankerModel, self).__init__()
         self.roberta = roberta
 
-    # def forward(
-    #         self,
-    #         input_ids=None,
-    #         attention_mask=None,
-    #         token_type_ids=None,
-    #         position_ids=None,
-    #         head_mask=None,
-    #         inputs_embeds=None,
-    #         labels=None,
-    #         output_attentions=None,
-    #         output_hidden_states=None,
-    #         return_dict=None,
-    # ):
-    def forward(self, **args):
-        input_ids_s_ = args['input_ids_s']  # shape(n_beam, tokenz_length)
-        res = self.roberta(input_ids_s_, args['attention_mask_s'])
+    def forward(
+            self,
+            input_ids_s,
+            attention_mask_s,
+            labels=None,
 
-        logits = res.logits.view(-1)
-        logits = (logits - logits.mean()) / (logits.std() + 0.01)
-        res['logits'] = logits
-        if 'labels' in args:
-            target = args['labels']
-            assert target.shape == logits.shape
-            loss = MSELoss(reduction='sum')(input=logits, target=target)
+    ):
+        res = self.roberta(input_ids_s, attention_mask_s)
+
+        if labels is not None:
+            logits = res.logits.view(-1)
+            assert labels.shape == logits.shape
+            labels = (labels - labels.mean()) / (labels.std() + 0.01)
+            loss = MSELoss()(input=logits, target=labels)
             res['loss'] = loss
 
         return res

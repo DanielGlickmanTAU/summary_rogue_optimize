59e542e9ad8676ff19dbcac9ac4aaa9f73d28f3e
Glickman <glickman1@mail.tau.ac.il>
2021-05-03 11:02:13 +0300

    ranking loss - tolerance

--- models/RankingLoss.py
+++ models/RankingLoss.py
@@ -1,23 +1,36 @@
 import torch.nn as nn
 import torch
 
-tolerance = 0.01
-reduction = 'sum'
-
 
 class RankingLoss(nn.Module):
+    def __init__(self, tolerance=0.01, reduction='sum'):
+        super(RankingLoss, self).__init__()
+        self.reduction = reduction
+        self.tolerance = tolerance
+
     def forward(self, logits, labels):
         assert logits.shape == labels.shape
         assert len(logits.shape) == 1  # assuming 2 vectors
 
         indices = labels.sort(descending=True).indices
-        sorted_logits = logits[indices]
+        # sorted_logits = logits[indices]
         differences = nn.Parameter(torch.tensor([]))
 
-        for i, logit_high in enumerate(sorted_logits[:-1]):
-            for j, logit_low in enumerate(sorted_logits[i + 1:]):
-                diff = logit_high - logit_low
-                if abs(diff) > tolerance:
-                    differences = torch.cat((differences, torch.tensor([diff])))
+        for i, high_index in enumerate(indices[:-1]):
+            for _, low_index in enumerate(indices[i + 1:]):
+                logit_high = logits[high_index]
+                logit_low = logits[low_index]
+                label_high = labels[high_index]
+                label_low = labels[low_index]
 
-        return differences.sigmoid().log()
+                logit_diff = logit_high - logit_low
+                label_diff = label_high - label_low
+                if abs(label_diff) > self.tolerance:
+                    differences = torch.cat((differences, torch.tensor([logit_diff])))
+
+        sigmoid_log_diffs = differences.sigmoid().log()
+        if self.reduction == 'sum':
+            return sigmoid_log_diffs.sum()
+        if self.reduction == 'mean':
+            return sigmoid_log_diffs.mean()
+        return sigmoid_log_diffs
--- models/test_RankingLoss.py
+++ models/test_RankingLoss.py
@@ -5,7 +5,7 @@ import torch
 
 
 class TestRankingLoss(TestCase):
-    def test_forward(self):
+    def test_forward_simple(self):
         loss = RankingLoss()
         logits = torch.tensor([1., 2.])
         labels = torch.tensor([0.1, 0.2])
@@ -19,6 +19,17 @@ class TestRankingLoss(TestCase):
         real_loss = loss(logits, labels)
         self.assertAlmostEqual(real_loss.item(), excepted_loss.item(), delta=0.001)
 
+    def test_forward_below_tolerance(self):
+        tolerance = 0.01
+        loss = RankingLoss(tolerance=tolerance)
+        logits = torch.tensor([1., 2.])
+        labels = torch.tensor([0.1, 0.1 + tolerance])
+
+        excepted_loss = torch.tensor([0.])
+        real_loss = loss(logits, labels)
+        self.assertAlmostEqual(real_loss.item(), excepted_loss.item(), delta=0.001)
+
 
 if __name__ == '__main__':
-    TestRankingLoss().test_forward()
+    TestRankingLoss().test_forward_simple()
+    TestRankingLoss().test_forward_below_tolerance()

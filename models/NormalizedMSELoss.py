734c3372b34a04cd831ad295bc310085b473f541
Glickman <glickman1@mail.tau.ac.il>
2021-05-08 15:14:30 +0300

    fix

--- models/NormalizedMSELoss.py
+++ models/NormalizedMSELoss.py
@@ -4,11 +4,11 @@ from torch.nn import MSELoss
 
 class NormalizedMSELoss(nn.Module):
 
-    def __init__(self, smoothing=0.0001, reduction='sum'):
+    def __init__(self, reduction='sum', smoothing=0.0001):
         super(NormalizedMSELoss, self).__init__()
-        self.reduction = reduction
         self.smoothing = smoothing
+        self.reduction = reduction
 
     def forward(self, logits, labels):
-        labels = (labels - labels.mean()) / (labels.std() + self.tolerance)
+        labels = (labels - labels.mean()) / (labels.std() + self.smoothing)
         return MSELoss(reduction=self.reduction)(input=logits, target=labels)

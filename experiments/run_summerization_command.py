5fc35e9c98cf75d011479f7a3d56a2e2e10d1b8e
Glickman <glickman1@mail.tau.ac.il>
2021-06-11 20:19:51 +0300

    commands

--- experiments/run_summerization_command.py
+++ experiments/run_summerization_command.py
@@ -2,21 +2,21 @@ from experiments.gridsearch import gridsearch
 from experiments.slurm import run_on_slurm
 
 params = {
-    'num_examples': 120,
-    'num_summaries_per_text': 32,
-    'learning_rate': 1e-5,
-    'gradient_accumulation_steps': 128,
-    'num_train_epochs': 60,
-    'loss_fn': 'rank-net',
-    'tolerance': 0.08,
-    'half_percision': False,
-    'do_evaluation': True,
-    # 'validation_mapped_saved_path': 'sshleifer_distilbart-xsum-12-3/processed_dataset__validation_xsum10000_do_sampleFalse_top_pNone_top_kNone_num_beams8_num_return_sequences8_no_repeat_ngram_size0',
-    # 'train_mapped_saved_path': 'sshleifer_distilbart-xsum-12-3/processed_dataset__train_xsum50000_do_sampleFalse_top_pNone_top_kNone_num_beams8_num_return_sequences8_no_repeat_ngram_size0'
-    'validation_mapped_saved_path': 'sshleifer_distilbart-cnn-12-3/processed_dataset__validation_xsum1000_do_sampleFalse_top_pNone_top_kNone_num_beams32_num_return_sequences32_no_repeat_ngram_size0',
-    'train_mapped_saved_path': 'sshleifer_distilbart-cnn-12-3/processed_dataset__train_xsum1000_do_sampleFalse_top_pNone_top_kNone_num_beams32_num_return_sequences32_no_repeat_ngram_size0',
-    # 200k
-    # 'train_mapped_saved_path': 'processed_dataset__train_xsum200000_do_sampleFalse_top_pNone_top_kNone_num_beams16_num_return_sequences16_no_repeat_ngram_size0'
+    'evaluation_strategy': 'epoch',
+    'max_predict_samples': '120',
+    'num_train_epochs': '30',
+    'per_device_train_batch_size': '4',
+    'per_device_eval_batch_size': '8',
+    'overwrite_output_dir': True,
+    'do_train': True,
+    'do_eval': True,
+    'max_train_samples': 4,
+    'do_predict': True,
+    'max_eval_samples': 64,
+    'num_beams': 4,
+    'model_name_or_path': 'facebook/bart-base',
+    'output_dir': './out',
+    'dataset_name': 'xsum'
 }
 
 params_for_grid_search = {

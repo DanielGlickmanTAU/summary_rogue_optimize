63374657c9cdb639ff422764c8c5f443123c5dff
Glickman <glickman1@mail.tau.ac.il>
2021-06-14 09:49:49 +0300

    generate using train bart that I trained

--- experiments/specific_generation_script.py
+++ experiments/specific_generation_script.py
@@ -5,21 +5,22 @@ from flows import loading
 from models import model_loading
 from models.generate import BeamSearchParams
 
-split = 'train'
+split = 'test'
 
-train_examples = 2_000
-validation_examples = 2_000
+train_examples = 200
+validation_examples = 128
+test_examples = 1000
 search_params = BeamSearchParams(num_beams=8, num_return_sequences=8)
 batch_size = 2
 
 # exp = experiment.start_experiment(hyperparams={'search': search_params,
 #                                                'batch_size': batch_size})
-# import websocket
+
+checkpoint_ = 'models/xsum/100/facebook/bart-base/1e-05'
+model, tokenizer = model_loading._get_bart_based_model_and_tokenizer(checkpoint_)
+# model, tokenizer = model_loading.get_bart_base_model_and_tokenizer()
+
+dataset = data_loading.get_xsum_dataset(train_subset=train_examples, valid_subset=validation_examples,
+                                        test_subset=test_examples)
 #
-# print('websocket version', websocket.__version__)
-# 1 / 0
-
-model, tokenizer = model_loading.get_bart_base_model_and_tokenizer()
-dataset = data_loading.get_xsum_dataset(train_subset=train_examples, valid_subset=validation_examples)
-
 search_validation_loss(dataset[split], model, tokenizer, search_params, batch_size)

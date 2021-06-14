0d7656245697a1d8b3f0235d50814597c3068cac
Glickman <glickman1@mail.tau.ac.il>
2021-06-14 10:28:49 +0300

    fix test

--- data/test_processing.py
+++ data/test_processing.py
@@ -13,8 +13,8 @@ class Test(TestCase):
     def test_convert_generated_summaries_dataset_to_regression_dataset_format(self):
         model, tokenizer = model_loading.get_bart_model_and_tokenizer_xsum()
         # mapped_saved_path = 'sshleifer_distilbart-xsum-12-3/processed_dataset__validation_xsum1200_do_sampleFalse_top_pNone_top_kNone_num_beams32_num_return_sequences32_no_repeat_ngram_size0'
-        # mapped_saved_path = 'sshleifer_distilbart-xsum-12-3/processed_dataset__validation_xsum408_do_sampleTrue_top_p0.9_top_kNone_num_beams8_num_return_sequences8_no_repeat_ngram_size0'
-        mapped_saved_path = 'processed_dataset__train_xsum50000_do_sampleFalse_top_pNone_top_kNone_num_beams8_num_return_sequences8_no_repeat_ngram_size0'
+        mapped_saved_path = 'sshleifer_distilbart-xsum-12-3/processed_dataset__validation_xsum408_do_sampleTrue_top_p0.9_top_kNone_num_beams8_num_return_sequences8_no_repeat_ngram_size0'
+        # mapped_saved_path = 'processed_dataset__train_xsum50000_do_sampleFalse_top_pNone_top_kNone_num_beams8_num_return_sequences8_no_repeat_ngram_size0'
         generated_xsum = generated_data_loading.load_generated_dataset(mapped_saved_path, 4)
         # generated_xsum = generated_xsum.select(range(10))
         processed_generated_xsum = processing.convert_generated_summaries_dataset_to_regression_dataset_format(

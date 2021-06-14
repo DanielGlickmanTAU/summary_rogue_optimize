6b0c10553ccf915c856bcc35043c73e91ab7d4f9
Glickman <glickman1@mail.tau.ac.il>
2021-06-14 18:07:59 +0300

    add loss function

--- models/loss/loss_factory.py
+++ models/loss/loss_factory.py
@@ -1,13 +1,17 @@
-from torch.nn import MSELoss
+from torch.nn import MSELoss, BCEWithLogitsLoss
 
+from config.config import RankerConfig
 from models.loss.MeanCenteredMSELoss import MeanCenteredMSELoss
 from models.loss.NormalizedMSELoss import NormalizedMSELoss
 from models.loss.RankNetLoss import RankNetLoss
 from models.loss.RankingLoss import RankingLoss
 
 
-def get_loss(config):
+def get_loss(config: RankerConfig):
     loss_fn = config['loss_fn'] if isinstance(config, dict) else config.loss_fn
+
+    if config.mode:
+        return BCEWithLogitsLoss()
     if loss_fn == 'centered-mse':
         return MeanCenteredMSELoss(reduction='mean')
     if loss_fn == 'normalized-mse':

ddf8472fa688ce84d502d7a133dbb403a8da9296
Glickman <glickman1@mail.tau.ac.il>
2021-06-18 11:33:50 +0300

    print also rouge-1 rougeL

--- evaluation/evaluate.py
+++ evaluation/evaluate.py
@@ -25,15 +25,16 @@ def print_rouge_stuff(ds):
     def mean_until(a, k):
         return a[:, 0:k + 1].max(axis=1).mean()
 
-    def print_for_rouge_type(ds, rouge_type):
+    def print_for_rouge_type(ds, rouge_type, all=True):
         scores = numpy.array(ds[('rouge-%s-all' % rouge_type)])  # list[list[float]
         bests = [mean_until(scores, k) for k in range(len(scores[0]))]
-        print('rouge-%s best at' % rouge_type, avg('rouge-%s-best' % rouge_type))
-        print('rouge-%s avg' % rouge_type, avg('rouge-%s-avg' % rouge_type))
-        print('rouge-%s first' % rouge_type, avg('rouge-%s-first' % rouge_type))
+        if all:
+            print('rouge-%s best at' % rouge_type, avg('rouge-%s-best' % rouge_type))
+            print('rouge-%s avg' % rouge_type, avg('rouge-%s-avg' % rouge_type))
+            print('rouge-%s first' % rouge_type, avg('rouge-%s-first' % rouge_type))
         print('rouge-%s-all' % rouge_type, bests)
 
     rouge_type = '2'
     print_for_rouge_type(ds, rouge_type)
-    print_for_rouge_type(ds, '1')
-    print_for_rouge_type(ds, 'L')
+    print_for_rouge_type(ds, '1', all=False)
+    print_for_rouge_type(ds, 'L', all=False)

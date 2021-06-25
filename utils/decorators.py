c368584a9015765eb419e084d9d61717539c4597
Glickman <glickman1@mail.tau.ac.il>
2021-06-25 10:36:20 +0300

    improved printing

--- utils/decorators.py
+++ utils/decorators.py
@@ -7,7 +7,7 @@ def measure_time(function):
         start = time.time()
         ret = function(*args, **kwargs)
         end = time.time()
-        print(f'{function.__name__} took {end - start} seconds')
+        print(f'*** {function.__name__} took {end - start} seconds ***\n')
         return ret
 
     return wrapper

016ab3626b0a15e950930a1938e208e88414ef53
Glickman <glickman1@mail.tau.ac.il>
2021-06-25 10:36:07 +0300

    improved printing

--- utils/decorators.py
+++ utils/decorators.py
@@ -3,11 +3,11 @@ import time
 
 def measure_time(function):
     def wrapper(*args, **kwargs):
-        print('STARTING ', function.__name__)
+        print(f'**** STARTING  {function.__name__} ****')
         start = time.time()
         ret = function(*args, **kwargs)
         end = time.time()
-        print(function.__name__, 'took ', end - start, 'seconds')
+        print(f'{function.__name__} took {end - start} seconds')
         return ret
 
     return wrapper

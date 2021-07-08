79424f3d251ef7365be69d1ca13ddf72e3d9bad4
Glickman <glickman1@mail.tau.ac.il>
2021-07-08 09:10:16 +0300

    generated summary cleaning

--- experiments/openai_dataset_reading.py
+++ experiments/openai_dataset_reading.py
@@ -3,11 +3,15 @@ import datasets
 import json
 import collections
 
-data = json.load(open('results_open_json.json'))
 
-d = collections.defaultdict(list)
-for x in data:
-    d['article'].append(x['text'])
-    d['generated_highlights'].append([x['summary']])
+def clean_summary(param):
+    return param.replace('\n', '')
 
-data = datasets.Dataset.from_dict(d)
+
+def get_generated_gpt_dataset():
+    data = json.load(open('results_open_json.json'))
+    d = collections.defaultdict(list)
+    for x in data:
+        d['article'].append(x['text'])
+        d['generated_highlights'].append([clean_summary(x['summary'])])
+    return datasets.Dataset.from_dict(d)

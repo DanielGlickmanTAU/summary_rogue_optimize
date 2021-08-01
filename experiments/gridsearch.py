6fd9e5a5feee334fc145964c3aff1162ffbddb58
Glickman <glickman1@mail.tau.ac.il>
2021-08-01 15:34:44 +0300

    only iter lists in gridsearch

--- experiments/gridsearch.py
+++ experiments/gridsearch.py
@@ -3,7 +3,16 @@ from itertools import product
 
 
 def gridsearch(default_params: Dict, params_to_grid_search: Dict[object, List]) -> List[Dict]:
-    params_as_dicts = [dict(zip(params_to_grid_search, v)) for v in product(*params_to_grid_search.values())]
+    def prod(params_to_grid_search):
+        values = []
+        for v in params_to_grid_search.values():
+            if isinstance(v, list):
+                values.append(v)
+            else:
+                values.append([v])
+        return product(*values)
+
+    params_as_dicts = [dict(zip(params_to_grid_search, v)) for v in prod(params_to_grid_search)]
     rets = []
     for d in params_as_dicts:
         ret = default_params.copy()
